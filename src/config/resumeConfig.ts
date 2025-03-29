dd4f7c4df490e1a433e3360311787f98fb6f166a
gammaSpeck <madhukm3141@gmail.com>
2025-03-29 15:53:42 +0530

    Cleaned code

--- src/config/resumeConfig.ts
+++ src/config/resumeConfig.ts
@@ -11,7 +11,7 @@ export const personalInfo = {
     "Tech aficionado with 5+ years of experience embracing the full-stack frenzy of " +
     new Date().getFullYear() +
     "—conceptualizing, R&D, coding, testing, and cloud deployment. A one-person IT army, reveling in the end-to-end adventure.",
-  pdfFilename: "madhu-km-resume.pdf", // This will be used for both download and generation
+  pdfFilename: "madhu-km-resume.pdf" // This will be used for both download and generation
 };
 
 // Contact Information
@@ -20,19 +20,19 @@ export const contactInfo = [
   {
     icon: "mdi-github",
     title: "gammaSpeck",
-    link: "https://github.com/gammaSpeck",
+    link: "https://github.com/gammaSpeck"
   },
   {
     icon: "mdi-linkedin",
     title: "madhusoodhanan-km",
-    link: "https://www.linkedin.com/in/madhusoodhanan-km",
+    link: "https://www.linkedin.com/in/madhusoodhanan-km"
   },
   {
     icon: "mdi-email",
     title: "tech@madhukm.com",
-    link: "mailto:tech@madhukm.com",
+    link: "mailto:tech@madhukm.com"
   },
-  { icon: "mdi-home", title: "Bengaluru, Karnataka, India" },
+  { icon: "mdi-home", title: "Bengaluru, Karnataka, India" }
 ];
 
 // Experience
@@ -40,28 +40,27 @@ export const experience = {
   sections: [
     {
       company: "Ollion",
-      summary:
-        "Worked extensively on DataPipes which is a cloud native Data Management Platform.",
+      summary: "Worked extensively on DataPipes which is a cloud native Data Management Platform.",
       positions: [
         {
           id: 1,
           title: "Lead Product Developer",
           timeline: "Apr 2023 - Present",
-          desc: "Orchestrated the end-to-end management of code and infrastructure for all front-end microservices, led a team of two skilled developers, completed the entire project migration from Vue 2 to Vue 3, and represented the product at international tech events to drive market visibility and engagement.",
+          desc: "Orchestrated the end-to-end management of code and infrastructure for all front-end microservices, led a team of two skilled developers, completed the entire project migration from Vue 2 to Vue 3, and represented the product at international tech events to drive market visibility and engagement."
         },
         {
           id: 2,
           title: "Senior Developer",
           timeline: "Apr 2022 - Apr 2023",
-          desc: "Owned multiple critical components, executed a comprehensive migration from Webpack to Vite, and collaborated closely with UX teams to enhance user experience and streamline front-end performance.",
+          desc: "Owned multiple critical components, executed a comprehensive migration from Webpack to Vite, and collaborated closely with UX teams to enhance user experience and streamline front-end performance."
         },
         {
           id: 3,
           title: "Developer",
           timeline: "May 2021 - Apr 2022",
-          desc: "Contributed to the migration from JavaScript to TypeScript, optimized containerization processes, developed multiple web pages, and authored numerous REST APIs for the backend.",
-        },
-      ],
+          desc: "Contributed to the migration from JavaScript to TypeScript, optimized containerization processes, developed multiple web pages, and authored numerous REST APIs for the backend."
+        }
+      ]
     },
     {
       company: "Cardinal Health",
@@ -71,36 +70,36 @@ export const experience = {
         {
           title: "Associate Software Engineer",
           timeline: "Jun 2019 - May 2021",
-          desc: "Built architectural frameworks in NodeJS and React.js, architected and deployed a project to Kubernetes while mentoring the team through the SDLC, designed and launched the MVP for an Admin Console used by clients and support teams, owned multiple components, and served as a technical interviewer for campus recruitment.",
+          desc: "Built architectural frameworks in NodeJS and React.js, architected and deployed a project to Kubernetes while mentoring the team through the SDLC, designed and launched the MVP for an Admin Console used by clients and support teams, owned multiple components, and served as a technical interviewer for campus recruitment."
         },
         {
           title: "Software Engineering Intern",
           timeline: "Jan 2019 - Jun 2021",
-          desc: "Developed microservices using the Serverless Framework and created a shared repository of reusable modules.",
-        },
-      ],
-    },
-  ],
+          desc: "Developed microservices using the Serverless Framework and created a shared repository of reusable modules."
+        }
+      ]
+    }
+  ]
 };
 
 // Languages
 export const languages = [
   {
     name: "English",
-    proficiency: "Advanced",
+    proficiency: "Advanced"
   },
   {
     name: "Hindi",
-    proficiency: "Proficient",
+    proficiency: "Proficient"
   },
   {
     name: "Tamil",
-    proficiency: "Intermediate",
+    proficiency: "Intermediate"
   },
   {
     name: "Malayalam",
-    proficiency: "Intermediate",
-  },
+    proficiency: "Intermediate"
+  }
 ];
 
 // Skills
@@ -109,7 +108,7 @@ export const skills = {
     { title: "Javascript", icon: "mdi-language-javascript" },
     { title: "Typescript", icon: "mdi-language-typescript" },
     { title: "Python", icon: "mdi-language-python" },
-    { title: "Shell", icon: "mdi-powershell" },
+    { title: "Shell", icon: "mdi-powershell" }
   ],
   frontend: [
     "Vue",
@@ -120,7 +119,7 @@ export const skills = {
     "MUI",
     "Cypress",
     "Playwright",
-    "Web Components",
+    "Web Components"
   ],
   backend: [
     "NodeJS",
@@ -133,19 +132,9 @@ export const skills = {
     "RDBMS",
     "Neo4j",
     "MongoDB",
-    "NATS",
+    "NATS"
   ],
-  devops: [
-    "AWS",
-    "CICD",
-    "Docker",
-    "Kubernetes",
-    "HELM",
-    "Terraform",
-    "Jenkins",
-    "k6",
-    "Grafana",
-  ],
+  devops: ["AWS", "CICD", "Docker", "Kubernetes", "HELM", "Terraform", "Jenkins", "k6", "Grafana"]
 };
 
 // Education
@@ -155,22 +144,22 @@ export const education = [
     grade: "CGPA 7.3/10",
     school: "CMRIT",
     from: "2015",
-    to: "2019",
+    to: "2019"
   },
   {
     degree: "PUC - PCMB",
     grade: "84%",
     school: "Christ Junior College",
     from: "2013",
-    to: "2015",
+    to: "2015"
   },
   {
     degree: "Secondary Education",
     grade: "92%",
     school: "St Thomas Public School",
     from: "",
-    to: "2013",
-  },
+    to: "2013"
+  }
 ];
 
 // Theme settings
@@ -178,5 +167,5 @@ export const themeSettings = {
   primaryColor: "#1867C0", // Default Vuetify primary color
   backgroundColor: "#fff",
   textColor: "#000",
-  accentColor: "#1867C0",
+  accentColor: "#1867C0"
 };
