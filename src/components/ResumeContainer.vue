101d2cecfcce973dd080a68d2e17c0554e702292
Zeel <zeelgor009@gmail.com>
2025-04-11 17:51:35 +0530

    Initial commit of resume project

--- src/components/ResumeContainer.vue
+++ src/components/ResumeContainer.vue
@@ -4,7 +4,7 @@
       <v-row class="justify-end">
         <v-tooltip text="Download Resume" location="bottom">
           <template #activator="{ props }">
-            <v-btn icon href="/resume.pdf" download class="ma-2" v-bind="props">
+            <v-btn icon href="/resume.pdf" download="Zeel_Resume.pdf" class="ma-2" v-bind="props">
               <v-icon>mdi-download</v-icon>
             </v-btn>
           </template>
@@ -12,27 +12,51 @@
       </v-row>
 
       <v-row>
-  <!-- LEFT COLUMN: Will take full width on mobile, 8 columns on medium+ screens -->
-  <v-col cols="12" md="8" class="d-flex flex-column ga-6">
-    <AboutSection />
-    <ExperienceSection />
-    <EducationSection />
-  </v-col>
+        <!-- Desktop View (md and up) -->
+        <template v-if="!mobile">
+          <!-- Left Column -->
+          <v-col cols="12" md="8" class="d-flex flex-column ga-6">
+            <AboutSection />
+            <ExperienceSection />
+            <EducationSection />
+          </v-col>
 
-  <!-- RIGHT SIDEBAR: Will take full width on mobile, 4 columns on medium+ screens -->
-  <v-col cols="12" md="4" class="d-flex flex-column align-center ga-4">
-    <v-img
-      src="/profile-photo.jpg"
-      alt="My Photo"
-      width="180"
-      height="180"
-      class="rounded"
-      contain
-    />
-    <SkillsSection />
-    <LanguagesSection />
-  </v-col>
-</v-row>
+          <!-- Right Sidebar -->
+          <v-col cols="12" md="4" class="d-flex flex-column align-center ga-4">
+            <v-img
+              src="/profile-photo.jpg"
+              alt="My Photo"
+              width="180"
+              height="180"
+              class="rounded"
+              contain
+            />
+            <SkillsSection />
+            <LanguagesSection />
+          </v-col>
+        </template>
+
+        <!-- Mobile View -->
+        <template v-else>
+          <v-col cols="12" class="d-flex flex-column align-center ga-4">
+            <v-img
+              src="/profile-photo.jpg"
+              alt="My Photo"
+              width="180"
+              height="180"
+              class="rounded"
+              contain
+            />
+          </v-col>
+          <v-col cols="12" class="d-flex flex-column ga-6">
+            <AboutSection />
+            <ExperienceSection />
+            <SkillsSection />
+            <EducationSection />
+            <LanguagesSection />
+          </v-col>
+        </template>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -50,24 +74,7 @@ import { themeSettings } from "../config/resumeConfig";
 
 const { mobile } = useDisplay({ mobileBreakpoint: 600 });
 
-const topColOrders = ref({ image: 2, info: 1 });
-
-const DEFAULT_BOTTOM_COL_SPAN = 7;
-const DEFAULT_TOP_COL_SPAN = 8;
-
-const leftSectionColSpan = ref(DEFAULT_BOTTOM_COL_SPAN);
-const topColSpan = ref(DEFAULT_TOP_COL_SPAN);
-
-function syncScreenSizeDimensions(isMobile: boolean) {
-  topColOrders.value = isMobile ? { image: 1, info: 2 } : { image: 2, info: 1 };
-  leftSectionColSpan.value = isMobile ? 12 : DEFAULT_BOTTOM_COL_SPAN;
-  topColSpan.value = isMobile ? 12 : DEFAULT_TOP_COL_SPAN;
-}
-
-watch(mobile, syncScreenSizeDimensions);
 onMounted(() => {
-  syncScreenSizeDimensions(mobile.value);
-
   document.documentElement.style.setProperty(
     "--primary-color",
     themeSettings.primaryColor
